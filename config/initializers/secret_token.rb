07ffda9005066bc6cf532fb0e309d94f28afd552
Bradley Herman <bradley.t.herman@gmail.com>
2010-12-20 13:58:07 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySoundboard::Application.config.secret_token = '48132add777d9e289eae4dd73b34e629d5c73a4740fc8f0ac3e5257f203a8f1d1423af3a9d29a62ac4010db89b99e96581c56642d9179c03889e713f0a101f3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MySoundboard::Application.config.session_store :cookie_store, :key => '_MySoundboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# MySoundboard::Application.config.session_store :active_record_store
