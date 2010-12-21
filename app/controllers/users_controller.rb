e7cb473d145ad658bce1990bfd358e46e8b8f24b
Bradley Herman <bradley.t.herman@gmail.com>
2010-12-21 02:35:16 -0500

    Several updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,7 @@
 class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
-  end
-
-  # GET /users/1
-  # GET /users/1.xml
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
+  def login
   end
 
   # GET /users/new
@@ -32,11 +15,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
   # POST /users
   # POST /users.xml
   def create
@@ -48,22 +26,6 @@ class UsersController < ApplicationController
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.xml
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :sounds
+  
+  validates_presence_of   :email, :password
+  validates_uniqueness_of :email
 end
